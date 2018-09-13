49860a56320ae0f59cb48143a24753b9907daa72
Simon Tovey <Simon.Tovey@epicgames.com>
2018-09-13 06:14:44 -0400

    Fixed GPU collision compiile issue.
    #rb none
    #fyi Uriel.Doyon, Shaun.Kime
    
    [CL 4364096 by Simon Tovey in Dev-Niagara branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xCD67C49F, 0x7DD448C4, 0x9285E0D3, 0x6D0BB43B);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xF109EF07, 0xE00C47AC, 0xB1DF217F, 0xAC0F1789);
